785a8640c1872cd600cd0bab57328282797f6244
jasmehra12 <jaskaran.1230j@gmail.com>
2025-05-07 11:39:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,39 +18,39 @@ from logging.handlers import RotatingFileHandler
 #rohit_1888 on Tg
 #--------------------------------------------
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8059030470:AAG8howtdf5jx_C4pXSfObiJ3LNcNf2ywok")
-APP_ID = int(os.environ.get("APP_ID", "22225430")) #Your API ID from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "4c5c28abd62233ef4b993fb972f83262") #Your API Hash from my.telegram.org
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7555140221:AAENuOYTsnlHK1qkb4kDOHh0Zevvg2FP8v8")
+APP_ID = int(os.environ.get("APP_ID", "28632755")) #Your API ID from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "06ac14fec2a56ee58691edb3ad09e7bf") #Your API Hash from my.telegram.org
 #--------------------------------------------
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002347428675")) #Your db channel Id
-OWNER = os.environ.get("OWNER", "Jas_Mehra") # Owner username without @
-OWNER_ID = int(os.environ.get("OWNER_ID", "6039119180")) # Owner id
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001864935335")) #Your db channel Id
+OWNER = os.environ.get("OWNER", "Cine_Fix") # Owner username without @
+OWNER_ID = int(os.environ.get("OWNER_ID", "7459181600")) # Owner id
 #--------------------------------------------
 PORT = os.environ.get("PORT", "8001")
 #--------------------------------------------
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Mrdaxx123:Mrdaxx123@cluster0.q1da65h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DATABASE_NAME", "JAS_MEHERA")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://RAMIJ:RAMIJ@ramij.b3qyu.mongodb.net/?retryWrites=true&w=majority&appName=Ramij")
+DB_NAME = os.environ.get("DATABASE_NAME", "Ramij")
 #--------------------------------------------
 FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "0"))  # 0 means no expiry
 BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/Anime_Nova_Chat_Group")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 #--------------------------------------------
-START_PIC = os.environ.get("START_PIC", "https://i.ibb.co/4RXmH8dm/x.jpg")
-FORCE_PIC = os.environ.get("FORCE_PIC", "https://i.ibb.co/r2ZC2YFT/x.jpg")
+START_PIC = os.environ.get("START_PIC", "https://i.ibb.co/j9K7NMSt/x.jpg")
+FORCE_PIC = os.environ.get("FORCE_PIC", "https://i.ibb.co/w9SryZf/x.jpg")
 
 #--------------------------------------------
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "Shortner.in")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "9849421c920a27a9b306f552cacf0560b903049a")
-TUT_VID = os.environ.get("TUT_VID","https://t.me/Moviezs_files_bot?start=Z2V0LTUwMTM1MTMxOTkzMTcxMDg")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "fa24cee4bfed6957a966890cb54540add7c28a1d")
+TUT_VID = os.environ.get("TUT_VID","https://t.me/hey_whatsup_bro")
 SHORT_MSG = "<b>⌯ Here is Your Download Link, Must Watch Tutorial Before Clicking On Download...</b>"
 
-SHORTENER_PIC = os.environ.get("SHORTENER_PIC", "https://i.ibb.co/4hBnnTG/x.jpg")
+SHORTENER_PIC = os.environ.get("SHORTENER_PIC", "https://i.ibb.co/8nLf0FSf/x.jpg")
 #--------------------------------------------
 
 #--------------------------------------------
-HELP_TXT = "<b><blockquote>ᴛʜɪs ɪs ᴀɴ ғɪʟᴇ ᴛᴏ ʟɪɴᴋ ʙᴏᴛ ᴡᴏʀᴋ ғᴏʀ @Nova_Networks\n\n❏ ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅs\n├/start : sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ\n├/about : ᴏᴜʀ Iɴғᴏʀᴍᴀᴛɪᴏɴ\n└/help : ʜᴇʟᴘ ʀᴇʟᴀᴛᴇᴅ ʙᴏᴛ\n\n sɪᴍᴘʟʏ ᴄʟɪᴄᴋ ᴏɴ ʟɪɴᴋ ᴀɴᴅ sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ ᴊᴏɪɴ ʙᴏᴛʜ ᴄʜᴀɴɴᴇʟs ᴀɴᴅ ᴛʀʏ ᴀɢᴀɪɴ ᴛʜᴀᴛs ɪᴛ.....!\n\n ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ <a href=https://t.me/Jas_Mehra>ᴊᴀs ᴍᴇʜʀᴀ</a></blockquote></b>"
-ABOUT_TXT = "<b><blockquote>◈ ᴄʀᴇᴀᴛᴏʀ: <a href=https://t.me/Jas_Mehra>ᴊᴀs ᴍᴇʜʀᴀ</a>\n◈ ꜰᴏᴜɴᴅᴇʀ ᴏꜰ : <a href=https://t.me/Nova_networks>ɴᴏᴠᴀ ɴᴇᴛᴡᴏʀᴋ</a>\n◈ ᴀɴɪᴍᴇ ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/Anime_x_Nova>ᴀɴɪᴍᴇ x ɴᴏᴠᴀ</a>\n◈ ᴍᴏᴠɪᴇs & sᴇʀɪᴇs ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/All_movies_worlds>ᴀʟʟ ᴍᴏᴠɪᴇs ᴡᴏʀʟᴅs</a>\n◈ ᴋᴅʀᴀᴍᴀ ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/Kdrama_Hindii_Dub>ᴋᴅʀᴀᴍᴀ ɪɴ ʜɪɴᴅɪ</a>\n◈ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href=https://t.me/Jas_Mehra>ᴊᴀs ᴍᴇʜʀᴀ</a></blockquote></b>"
+HELP_TXT = "<b><blockquote>ᴛʜɪs ɪs ᴀɴ ғɪʟᴇ ᴛᴏ ʟɪɴᴋ ʙᴏᴛ ᴡᴏʀᴋ ғᴏʀ @Cine_Fix\n\n❏ ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅs\n├/start : sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ\n├/about : ᴏᴜʀ Iɴғᴏʀᴍᴀᴛɪᴏɴ\n├/help : ʜᴇʟᴘ ʀᴇʟᴀᴛᴇᴅ ʙᴏᴛ\n\n sɪᴍᴘʟʏ ᴄʟɪᴄᴋ ᴏɴ ʟɪɴᴋ ᴀɴᴅ sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ ᴊᴏɪɴ ʙᴏᴛʜ ᴄʜᴀɴɴᴇʟs ᴀɴᴅ ᴛʀʏ ᴀɢᴀɪɴ ᴛʜᴀᴛs ɪᴛ.....!\n\n ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ <a href=https://t.me/Jas_Mehra>ᴊᴀs ᴍᴇʜʀᴀ</a></blockquote></b>"
+ABOUT_TXT = "<b><blockquote>◈ ᴄʀᴇᴀᴛᴏʀ: <a href=https://t.me/Jas_Mehra>ᴊᴀs ᴍᴇʜʀᴀ</a>\n◈ ꜰᴏᴜɴᴅᴇʀ ᴏꜰ : <a href=https://t.me/ movie_flix_india>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>\n◈ ᴍᴏᴠɪᴇs & sᴇʀɪᴇs ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/movie_flix_india>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>\n◈ ᴋᴅʀᴀᴍᴀ ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/Kdrama_Hindii_Dub>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>\n◈ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href=https://t.me/Jas_Mehra>ᴊᴀs ᴍᴇʜʀᴀ</a></blockquote></b>"
 #--------------------------------------------
 #--------------------------------------------
 START_MSG = os.environ.get("START_MESSAGE", "<b>ʜᴇʟʟᴏ {first}\n\n<blockquote> ɪ ᴀᴍ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ, ɪ ᴄᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ɪᴛ ғʀᴏᴍ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ.</blockquote></b>")
@@ -79,7 +79,7 @@ CMD_TXT = """<blockquote><b>» ᴀᴅᴍɪɴ ᴄᴏᴍᴍᴀɴᴅs:</b></blockqu
 <b>›› /count :</b> ᴄᴏᴜɴᴛ verifications
 """
 #--------------------------------------------
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>• ʙʏ @nova_flix</b>") #set your Custom Caption here, Keep None for Disable Custom Caption
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "") #set your Custom Caption here, Keep None for Disable Custom Caption
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False #set True if you want to prevent users from forwarding files from bot
 #--------------------------------------------
 #Set true if you want Disable your Channel Posts Share button
@@ -90,7 +90,7 @@ USER_REPLY_TEXT = "ʙᴀᴋᴋᴀ ! ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ ꜱᴇɴᴘ
 
 #==========================(BUY PREMIUM)====================#
 
-OWNER_TAG = os.environ.get("OWNER_TAG", "Jas_Mehra")
+OWNER_TAG = os.environ.get("OWNER_TAG", "Cine_Fix")
 UPI_ID = os.environ.get("UPI_ID", "jaskaranmehra@fam")
 QR_PIC = os.environ.get("QR_PIC", "https://i.ibb.co/r2XD1j60/x.jpg")
 SCREENSHOT_URL = os.environ.get("SCREENSHOT_URL", f"t.me/Jas_Mehra")
