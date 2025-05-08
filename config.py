5cd10dea3dc2937a5936423e5445552d69835d2a
jasmehra12 <jaskaran.1230j@gmail.com>
2025-05-08 15:30:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,8 +29,8 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6617544956")) # Owner id
 #--------------------------------------------
 PORT = os.environ.get("PORT", "8001")
 #--------------------------------------------
-DB_URI = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("DATABASE_NAME", "")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Priyanshu825:<db_password>@cluster0.iazpu79.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DATABASE_NAME", "Priyanshu825")
 #--------------------------------------------
 FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "0"))  # 0 means no expiry
 BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/Anime_Nova_Chat_Group")
